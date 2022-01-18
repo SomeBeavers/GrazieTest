77fd4fcfeb859d12d491847e435b0cfbd6248317
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-01-18 16:48:51 +0300

    Add shared project.

---
+++ SharedProject/Shared1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject
+{
+    internal class Shared1
+    {
+        // Test my comment
+
+#if NET6_0
+        // Tst my comment
+
+        public void Tst()
+        {
+        }
+#else
+        // Tst my comment
+
+        public void Tst()
+        {
+        }
+#endif
+    }
+}

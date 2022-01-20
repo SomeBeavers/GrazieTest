60d12a45cf5ac37d63ffa3ab39d15d8962d266b2
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-01-20 13:36:38 +0300

    Add multiline comment.

--- net6_Console/languages/Languages.cs
+++ net6_Console/languages/Languages.cs
@@ -32,6 +32,7 @@ public class Languages
      *
      * typo:
      * ощень сложно
+     * Локалная
      *
          * Deutsch (Deutschland)
      * ---------------------
---
+++ net6_Console/languages/MultilangComment.cs
@@ -0,0 +1,53 @@
+﻿namespace net6_Console;
+
+public class MultilangComment
+{
+    /*
+     * English(Canada)
+     * ---------------------
+     * no typo:
+     * theatre
+     *
+     * typo:
+     * theater
+     *
+     * English(Great Britain)
+     * ---------------------
+     * no typo:
+     * travelling
+     * flavour
+     *
+     * typo:
+     * 	traveling
+     *
+     * Русский
+     * ---------------------
+     * no typo:
+     * ох, русский сложно
+     *
+     * typo:
+     * ощень сложно ←-----------------------------------------
+     * Локалная
+     *
+     * English(Canada)
+     * ---------------------
+     * no typo:
+     * theatre
+     *
+     * typo:
+     * theater
+     *
+     * English(Great Britain)
+     * ---------------------
+     * no typo:
+     * travelling
+     * flavour
+     *
+     * typo:
+     * 	traveling
+     *
+     *coment
+     */
+
+    // ощень сложно ←-----------------------------------------
+}

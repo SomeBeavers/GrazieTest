08427a70d236fe7172205bcb273228cf64fc76bb
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-01-20 10:06:12 +0300

    Add WinForms

---
+++ Core_MVC/ts.js
@@ -0,0 +1,9 @@
+function mathTest(num) {
+    var squareRoot = Math.sqrt(num);
+    console.log("Random Number:  " + num);
+    console.log("Square root:  " + squareRoot);
+}
+var num = Math.random();
+// num
+mathTest(num);
+//# sourceMappingURL=ts.js.map

bf1163b797594b5cd62bb7a1591b5ad537704b67
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-01-20 10:01:27 +0300

    Update.

---
+++ Core_MVC/ts.ts
@@ -0,0 +1,8 @@
+﻿function mathTest(num: number): void {
+    var squareRoot = Math.sqrt(num);
+    console.log("Random Number:  " + num);
+    console.log("Square root:  " + squareRoot);
+}
+var num: number = Math.random();
+// num
+mathTest(num);

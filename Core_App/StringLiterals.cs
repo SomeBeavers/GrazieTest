3602417893894809fc8785c294cd0deb13467f47
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-01-20 15:12:35 +0100

    Update.

---
+++ Core_App/StringLiterals.cs
@@ -0,0 +1,35 @@
+﻿// Lilia.Shamsutdinova
+// GrazieTest
+// Core_App
+// StringLiterals.cs
+
+namespace Core_App;
+
+public class StringLiterals
+{
+    public void StringLiterals1()
+    {
+        var string1 = "in future we will do better for sure!!!!";
+        var string2 = @"lets
+check how opened string 
+is handled
+";
+        var string3=
+        "lets test here too\r\ntest\r\n. lets test again";
+        
+        var string4=
+        """    
+        lets test here too
+        test
+
+        """;
+        string iMBitWeakToday = "I'm bit weak today.";
+
+        var string5 = $"I'm bit weak today. {iMBitWeakToday}'m bit weak today";
+        Console.WriteLine("I'm bit weak today. {0}'m bit weak today", "");
+
+        string.Format("I'm bit weak today. {0}'m bit weak today", "");
+
+        // I'm bit weak today.
+    }
+}

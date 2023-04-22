35ae115ec08aaddc9c5dc9c09051d816f503a240
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-04-22 14:03:41 +0200

    UPD.

--- Core_App/Program.cs
+++ Core_App/Program.cs
@@ -8,3 +8,17 @@ Console.WriteLine("hello my dear World!");
 // русский комментарий
 // русский комментарий
 // I'm bit weak today.
+// I'm bit weak today.
+// the my car is in garags.
+
+class MyClass
+{
+
+    /// <summary>
+    /// I'm bit weak today.
+    /// </summary>
+    public void MyMethod()
+    {
+  
+    }
+}

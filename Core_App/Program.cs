3e6e40817fb29e7174c645df871d87d16ff1e7d3
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-04-22 12:02:29 +0200

    UPD

--- Core_App/Program.cs
+++ Core_App/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("hello my dear World!");
 
 // русский комментарий
+// русский комментарий
+// русский комментарий
+// русский комментарий
+// русский комментарий
+// I'm bit weak today.

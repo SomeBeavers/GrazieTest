bf11caaaa6ebd7aea23d0c9987f4950b66e99968
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-05-11 14:32:03 +0200

    Add languages.

--- net6_Console/languages/Polski.cs
+++ net6_Console/languages/Polski.cs
@@ -1,13 +1,18 @@
-﻿namespace net6_Console.languages;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace net6_Console.languages;
 
 public class Polski
 {
     /*
+    /*
      * Teraz nie mogę dnia zacząc beze tego
      */
 
+    public const string s1 = "Teraz nie mogę dnia zacząc beze tego";
+
     public void Test()
     {
-
+        var s = "Teraz nie mogę dnia zacząc beze tego";
     }
 }
---
+++ net6_Console/languages/Romania.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace net6_Console.languages;
+
+public class Romania
+{
+    // Ei conviețuiau laolaltă acolo. test
+    public const string s = "Ei conviețuiau laolaltă acolo. test";
+
+    // Het stuk die ik speel is mooi.
+    public const string s2 = "Het stuk die ik speel is mooi.";
+
+    // I'm bit of today so worry
+    public const string s3 = "I'm bit of today so worry";
+}

0951bcf1012d02335d85d9074484d159af29475e
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-05-11 14:17:33 +0200

    Add dictionaries.

---
+++ net6_Console/languages/Polski.cs
@@ -0,0 +1,13 @@
+﻿namespace net6_Console.languages;
+
+public class Polski
+{
+    /*
+     * Teraz nie mogę dnia zacząc beze tego
+     */
+
+    public void Test()
+    {
+
+    }
+}
---
+++ net6_Console/languages/Portugues-Angola.cs
@@ -0,0 +1,15 @@
+﻿namespace net6_Console.languages;
+
+public class Portugues_Angola
+{
+    // Aquelle artigo é mais bom que o outro.
+    // Aquele artigo é mais bom que o outro.
+    // CãoEGatoÉAnimaassi
+    public void Test()
+    {
+        // ReSharper disable once ConvertToConstant.Local
+        string cãoEGatoÉAnimaassi = "Cão e gato é animais.";
+
+        Console.WriteLine(cãoEGatoÉAnimaassi);
+    }
+}

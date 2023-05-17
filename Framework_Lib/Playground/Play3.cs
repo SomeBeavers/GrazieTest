f004aeb16173f63175bf2f6b6d9de84bd4a0401a
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-05-17 16:13:37 +0200

    Add file.

---
+++ Framework_Lib/Playground/Play3.cs
@@ -0,0 +1,10 @@
+﻿namespace Framework_Lib.Playground
+{
+    /// <summary>
+    ///             Sucht eine lokalisierte Zeichenfolge, die "The flag &apos;DateTimeStyles.NoCurrentDateDefault&apos; is not supported." ähnelt.
+    /// </summary>
+    public class Play3
+    {
+        //            Sucht eine lokalisierte Zeichenfolge, die "The flag &apos;DateTimeStyles.NoCurrentDateDefault&apos; is not supported." ähnelt. 
+    }
+}

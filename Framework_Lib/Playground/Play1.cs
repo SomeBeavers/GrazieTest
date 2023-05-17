c417065a19f65c32c676a65fc90397f8faa9a16e
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-05-17 12:12:31 +0200

    Remove dictionaries. Play.

---
+++ Framework_Lib/Playground/Play1.cs
@@ -0,0 +1,11 @@
+﻿namespace Framework_Lib.Playground
+{
+    public class Play1
+    {
+        public void Test()
+        {
+            // comment. the name is 你好.
+            string s = "the name is Test";
+        }
+    }
+}
